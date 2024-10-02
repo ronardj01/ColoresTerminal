79f936c8f31e291643c95e4d4cdd39ea92489fef
ronardj01 <ronardj01@gmail.com>
2024-10-02 09:50:39 +0200

    como determinar sistema operativo

--- Colores.java
+++ Colores.java
@@ -1,13 +1,16 @@
-public class Colores{
+public class Colores {
     public static void main(String arg[]) {
 
-    /*Los colores de texto son código \033[3Xm y \033[9Xm la X va del 1 al 7
+   /*Los colores de texto son código \033[3Xm y \033[9Xm la X va del 1 al 7
       Los colores de fondo son código \033[4Xm y \033[10Xm la X va del 1 al 7
       Los colores extendidos son código (Texto: \033[38;5;Xm) y (Fondo: \033[48;5;Xm) la X va de 0 a 255
       Para la negritas \033[1m.
       Para restablecer todos los valores \033[0m
       *** Es posible convinar negrita, texto y fondo el orden es importante \033[1;3X;4Xm 
             para colores extendidos \033[1;38;5;X;48;5;Xm ***
+        *** Esto fuciona para Linux y Mac para windows lo ideal es usar una libreria como "JANSI"
+        para saber en que sistema operativo se esta ejecutando java podemos usar 
+        System.getProperty("os.name") y System.getProperty("os.arch")
     */
 
     System.out.println("");
@@ -23,25 +26,26 @@ public class Colores{
         
 
     }
-        public static String[] getColor() {
-            String negro = getColorCodeString("NEGRO", 30);
-            String rojo = getColorCodeString("ROJO", 31);
-            String verde = getColorCodeString("VERDE", 32);
-            String amarillo = getColorCodeString("AMARILLO", 33);
-            String azul = getColorCodeString("AZUL", 34);
-            String morado = getColorCodeString("MORADO", 35);
-            String cian = getColorCodeString("CIAN", 36);
-            String blanco = getColorCodeString("BLANCO", 37);
 
-         String result[] = {negro, rojo, verde, amarillo, azul, morado, cian, blanco};
-         return result; 
-        }
+    public static String[] getColor() {
+        String negro = getColorCodeString("NEGRO", 30);
+        String rojo = getColorCodeString("ROJO", 31);
+        String verde = getColorCodeString("VERDE", 32);
+        String amarillo = getColorCodeString("AMARILLO", 33);
+        String azul = getColorCodeString("AZUL", 34);
+        String morado = getColorCodeString("MORADO", 35);
+        String cian = getColorCodeString("CIAN", 36);
+        String blanco = getColorCodeString("BLANCO", 37);
 
-        public static String getColorCodeString(String color, int codigo) {
-            String result;
-            int codigoClaro = codigo + 60;
-            result = String.format("\033[" + codigo + "m    %-14s      %10d\033[0m", color, codigo) + 
-                     String.format("\033[" + codigoClaro + "m    %14s CLARO      %10d\033[0m", color, codigoClaro);
-            return result;
-        }
+        String result[] = { negro, rojo, verde, amarillo, azul, morado, cian, blanco };
+        return result;
+    }
+
+    public static String getColorCodeString(String color, int codigo) {
+        String result;
+        int codigoClaro = codigo + 60;
+        result = String.format("\033[" + codigo + "m    %-14s      %10d\033[0m", color, codigo) +
+                String.format("\033[" + codigoClaro + "m    %14s CLARO      %10d\033[0m", color, codigoClaro);
+        return result;
+    }
 }
